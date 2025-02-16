30b61729327def7cde52795f6ddf2fa9c937b3d6
sachiniwathudura <sachiniwathudura@gmail.com>
2025-02-16 23:20:33 +0530

    create navbar

---
+++ src/components/Navbar/PrivateNavbar.tsx
@@ -0,0 +1,116 @@
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { IoLogOutOutline } from "react-icons/io5";
+import { SiAuthy } from "react-icons/si";
+import { logoutAction } from "../../redux/slice/authSlice";
+
+function classNames(...classes: (string | boolean)[]): string {
+    return classes.filter(Boolean).join(" ");
+}
+
+const PrivateNavbar: React.FC = () => {
+    const dispatch = useDispatch();
+
+    const logoutHandler = () => {
+        dispatch(logoutAction());
+        localStorage.removeItem("userInfo");
+    };
+
+    return (
+        <Disclosure as="nav" className="bg-white">
+            {({ open }) => (
+                <>
+                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <div className="flex h-16 justify-start items-center">
+                            <div className="flex justify-center flex-row w-full">
+                                <div className="-ml-2 mr-2 flex items-left md:hidden">
+                                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                                        <span className="absolute -inset-0.5" />
+                                        <span className="sr-only">Open main menu</span>
+                                        {open ? (
+                                            <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                                        ) : (
+                                            <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                                        )}
+                                    </Disclosure.Button>
+                                </div>
+                                <div className="flex flex-shrink-0 items-center">
+                                    <SiAuthy className="h-8 w-auto text-green-500" />
+                                </div>
+                                <div className="hidden md:ml-6 md:flex md:space-x-8">
+                                    <Link to="/" className="inline-flex items-center border-b-2 border-indigo-500 px-1 pt-1 text-sm font-medium text-gray-900">
+                                        MasyncTracker
+                                    </Link>
+                                </div>
+                                <div className="hidden md:ml-6 md:flex md:space-x-8">
+                                    {["add-transaction", "add-category", "categories", "profile", "dashboard"].map((route) => (
+                                        <Link
+                                            key={route}
+                                            to={`/${route}`}
+                                            className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                                        >
+                                            {route.replace("-", " ")}
+                                        </Link>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="flex items-center">
+                                <button
+                                    onClick={logoutHandler}
+                                    type="button"
+                                    className="relative m-2 inline-flex items-center gap-x-1.5 rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600"
+                                >
+                                    <IoLogOutOutline className="h-5 w-5" aria-hidden="true" />
+                                    <span>Logout</span>
+                                </button>
+                                <Menu as="div" className="relative ml-1">
+                                    <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                        <span className="absolute -inset-1.5" />
+                                        <span className="sr-only">Open user menu</span>
+                                    </Menu.Button>
+                                    <Transition
+                                        as={Fragment}
+                                        enter="transition ease-out duration-200"
+                                        enterFrom="transform opacity-0 scale-95"
+                                        enterTo="transform opacity-100 scale-100"
+                                        leave="transition ease-in duration-75"
+                                        leaveFrom="transform opacity-100 scale-100"
+                                        leaveTo="transform opacity-0 scale-95"
+                                    >
+                                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <Link
+                                                        to="/student-dashboard"
+                                                        className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}
+                                                    >
+                                                        My Dashboard
+                                                    </Link>
+                                                )}
+                                            </Menu.Item>
+                                            <Menu.Item>
+                                                {({ active }) => (
+                                                    <button
+                                                        onClick={logoutHandler}
+                                                        className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm text-gray-700")}
+                                                    >
+                                                        Sign out
+                                                    </button>
+                                                )}
+                                            </Menu.Item>
+                                        </Menu.Items>
+                                    </Transition>
+                                </Menu>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )}
+        </Disclosure>
+    );
+};
+
+export default PrivateNavbar;
