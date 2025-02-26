ba2e5dbedb7bdaa0bbdab9f373135366bf0cb095
sachiniwathudura <sachiniwathudura@gmail.com>
2025-02-26 18:32:39 +0530

    update - user profile

--- src/components/Users/UpdatePassword.tsx
+++ src/components/Users/UpdatePassword.tsx
@@ -8,10 +8,24 @@ import { changePasswordAPI } from "../../services/users/userService";
 import { logoutAction } from "../../redux/slice/authSlice";
 import AlertMessage from "../Alert/AlertMessage";
 
-interface FormValues {
+// Define types
+interface UpdatePasswordFormValues {
     password: string;
 }
 
+interface ChangePasswordResponse {
+    data: any;
+}
+
+interface ChangePasswordError {
+    response: {
+        data: {
+            message: string;
+        };
+    };
+}
+
+// Validation schema
 const validationSchema = Yup.object({
     password: Yup.string()
         .min(5, "Password must be at least 5 characters long")
@@ -19,28 +33,26 @@ const validationSchema = Yup.object({
 });
 
 const UpdatePassword: React.FC = () => {
-    // Dispatch
     const dispatch = useDispatch();
 
-    // Mutation
-    const { mutateAsync, isPending, isError, error, isSuccess } = useMutation({
+    const { mutateAsync, isPending, isError, error, isSuccess } = useMutation<
+        ChangePasswordResponse,
+        ChangePasswordError,
+        string
+    >({
         mutationFn: changePasswordAPI,
         mutationKey: ["change-password"],
     });
 
-    const formik = useFormik<FormValues>({
+    const formik = useFormik<UpdatePasswordFormValues>({
         initialValues: {
             password: "",
         },
-        // Validations
         validationSchema,
-        // Submit
         onSubmit: (values) => {
-            mutateAsync(values.password)
+            mutateAsync({ newPassword: values.password })
                 .then(() => {
-                    // Logout
                     dispatch(logoutAction());
-                    // Remove the user from storage
                     localStorage.removeItem("userInfo");
                 })
                 .catch((e) => console.log(e));
@@ -48,47 +60,38 @@ const UpdatePassword: React.FC = () => {
     });
 
     return (
-        <div className="flex flex-col items-center justify-center p-4">
-            <h2 className="text-lg font-semibold mb-4">Change Your Password</h2>
+        <div className="flex flex-col items-center justify-center p-6 bg-[#F8F9FA] rounded-lg shadow-md">
+            <h2 className="text-xl font-semibold text-[#9C27B0] mb-4">Change Your Password</h2>
             <form onSubmit={formik.handleSubmit} className="w-full max-w-xs">
                 <div className="mb-4">
-                    <label
-                        className="block text-sm font-medium mb-2"
-                        htmlFor="new-password"
-                    >
+                    <label className="block text-sm font-medium text-[#2C3E50] mb-2" htmlFor="new-password">
                         New Password
                     </label>
                     {isPending && <AlertMessage type="loading" message="Updating...." />}
                     {isError && (
-                        <AlertMessage type="error" message={(error as any).response.data.message} />
+                        <AlertMessage type="error" message={(error as ChangePasswordError).response.data.message} />
                     )}
                     {isSuccess && (
-                        <AlertMessage
-                            type="success"
-                            message="Password updated successfully"
-                        />
+                        <AlertMessage type="success" message="Password updated successfully" />
                     )}
-                    <div className="flex items-center border-2 py-2 px-3 rounded">
-                        <AiOutlineLock className="text-gray-400 mr-2" />
+                    <div className="flex items-center border-2 border-[#9C27B0] py-2 px-3 rounded">
+                        <AiOutlineLock className="text-[#2C3E50] mr-2" />
                         <input
                             id="new-password"
                             type="password"
-                            name="newPassword"
+                            name="password"
                             {...formik.getFieldProps("password")}
-                            className="outline-none flex-1"
+                            className="outline-none flex-1 bg-transparent text-[#2C3E50]"
                             placeholder="Enter new password"
                         />
                     </div>
                     {formik.touched.password && formik.errors.password && (
-                        <span className="text-xs text-red-500">
-              {formik.errors.password}
-            </span>
+                        <span className="text-xs text-[#E74C3C]">{formik.errors.password}</span>
                     )}
                 </div>
-
                 <button
                     type="submit"
-                    className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="w-full bg-[#9C27B0] hover:bg-[#BA68C8] text-white font-bold py-2 px-4 rounded transition"
                 >
                     Update Password
                 </button>
--- src/components/Users/UserProfile.tsx
+++ src/components/Users/UserProfile.tsx
@@ -36,7 +36,7 @@ const UserProfile: React.FC = () => {
     return (
         <>
             <div className="max-w-4xl mx-auto my-10 p-8 bg-white rounded-lg shadow-md">
-                <h1 className="mb-2 text-2xl text-center font-extrabold">
+                <h1 className="mb-2 text-3xl text-center font-extrabold">
                     Welcome
                     {/* <span className="text-gray-500 text-sm ml-2">info@gmail.com</span> */}
                 </h1>
@@ -54,7 +54,7 @@ const UserProfile: React.FC = () => {
                 <form onSubmit={formik.handleSubmit} className="space-y-6">
                     {/* User Name Field */}
                     <div className="flex items-center space-x-4">
-                        <FaUserCircle className="text-3xl text-gray-400" />
+                        <FaUserCircle className="text-3xl text-[#9C27B0]" />
                         <div className="flex-1">
                             <label
                                 htmlFor="username"
@@ -66,7 +66,7 @@ const UserProfile: React.FC = () => {
                                 {...formik.getFieldProps("username")}
                                 type="text"
                                 id="username"
-                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                className="mt-1 block w-full border-2 border-[#9C27B0] rounded-md shadow-sm py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                                 placeholder="Your username"
                             />
                         </div>
@@ -78,8 +78,8 @@ const UserProfile: React.FC = () => {
                     </div>
 
                     {/* Email Field */}
-                    <div className="flex items-center space-x-4">
-                        <FaEnvelope className="text-3xl text-gray-400" />
+                    <div className="flex items-center space-x-4 ">
+                        <FaEnvelope className="text-3xl text-[#9C27B0]" />
                         <div className="flex-1">
                             <label
                                 htmlFor="email"
@@ -91,7 +91,7 @@ const UserProfile: React.FC = () => {
                                 type="email"
                                 id="email"
                                 {...formik.getFieldProps("email")}
-                                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                                className="mt-1 block w-full border-2 border-[#9C27B0] rounded-md shadow-sm py-2 px-4 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                                 placeholder="Your email"
                             />
                         </div>
@@ -106,7 +106,7 @@ const UserProfile: React.FC = () => {
                     <div className="flex justify-end mt-6">
                         <button
                             type="submit"
-                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                            className="bg-[#9C27B0] hover:bg-[#BA68C8] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         >
                             Save Changes
                         </button>
